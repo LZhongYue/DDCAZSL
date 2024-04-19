18820e67ec1ac43393d34f8096a936184fc3e299
LZhongYue <66252224+LZhongYue@users.noreply.github.com>
2024-04-19 22:03:26 +0800

    Update train_FLO_finally.py

--- train_FLO_finally.py
+++ train_FLO_finally.py
@@ -48,17 +48,12 @@ parser.add_argument('--save_interval', type=int, default=10000)
 parser.add_argument('--evl_interval',  type=int, default=50)
 parser.add_argument('--manualSeed', type=int, default=6152, help='manual seed')
 parser.add_argument('--input_dim',     type=int, default=1024, help='dimension of the global semantic vectors')
-###########
-#术高莫用
-############
+
 parser.add_argument('--gammaD', type=int, default=10, help='weight on the W-GAN loss')
 parser.add_argument('--gammaG', type=float, default=0.001, help='weight on the W-GAN loss')
 parser.add_argument('--lr_d', type=float, default=0.0001, help='learning rate to train discriminator')
 parser.add_argument('--lambda1', type=float, default=10, help='gradient penalty regularizer, following WGAN-GP')
 
-###########
-#术高莫用
-############
 parser.add_argument('--prototype',    type=float, default=1, help='weight of the prototype loss')
 parser.add_argument('--cprototype',    type=float, default=1, help='weight of the prototype loss')#0.1
 parser.add_argument('--cons',    type=float, default=0.01, help='weight of the prototype loss')
@@ -139,7 +134,7 @@ def train():
     optimizerD = optim.Adam(netDis.parameters(), lr=opt.lr_d, betas=(0.5, 0.999))
     with open(log_dir, 'a') as f:
         f.write('\n')
-        f.write('时间开始加速了 Training Start:')
+        f.write('Training Start:')
         f.write(strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime()) + '\n')
     start_step = 0
 
